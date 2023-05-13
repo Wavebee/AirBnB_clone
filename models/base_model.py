d68aa1ebe389a921a89205016f5a07f3579b544c
Alejio20 <humbleotamiri@gmail.com>
2023-05-13 07:41:33 -0700

    Added base_model

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+"""Base model for all other classes"""
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """BaseModel class definition"""
+
+    def __init__(self):
+        """Initialize the BaseModel class"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """print() & str() representation of BaseModel"""
+
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+
+        base_dict = self.__dict__
+        base_dict["__class__"] = type(self).__name__
+        base_dict["created_at"] = base_dict["created_at"].isoformat()
+        base_dict["updated_at"] = base_dict["updated_at"].isoformat()
+
+        return base_dict
